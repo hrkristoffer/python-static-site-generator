044286f49881925d7a5b1028c50f2df4a6558f0e
Kristoffer Johansen <hr.kristoffer@gmail.com>
2021-06-09 22:31:49 +0200

    Parser Sub-Classes Complete

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,6 +1,10 @@
+import sys
 from typing import List
 from pathlib import Path
 import shutil
+from docutils.core import publish_parts
+from markdown import markdown
+from ssg.content import Content
 
 
 class Parser:
@@ -14,7 +18,7 @@ class Parser:
         raise NotImplementedError
 
     def read(self, path: Path):
-         with open(path, 'r') as file:
+        with open(path, 'r') as file:
             return file.read()
 
     def write(self, path: Path, dest: Path, content, ext='.html'):
@@ -32,3 +36,25 @@ class ResourceParser(Parser):
 
     def parse(self, path, source, dest):
         self.copy(path, source, dest)
+
+
+class MarkdownParser(Parser):
+
+    extensions = ['.md', '.markdown']
+
+    def parse(self, path, source, dest):
+        content = Content.load(self.read(path))
+        html = markdown(content.body)
+        self.write(path, dest, html)
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
+
+
+class ReStructuredTextParser(Parser):
+
+    extensions = ['.rst']
+
+    def parse(self, path, source, dest):
+        content = Content.load(self.read(path))
+        html = publish_parts(content.body, writer_name='html5')
+        self.write(path, dest, html['html_body'])
+        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content))
